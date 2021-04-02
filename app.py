1a491c5679ae8e3b5e6a893b8a23f195d5f56d9e
ecalzo <evancalz@me.com>
2021-04-02 09:36:27 -0400

    Changes to main App * cleanup var names, use url instead of link * add reddit_make_post * better logging * use .env file when running locally

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask, request
-from xml_parser import parse_yt_xml
+from utils.xml_parser import parse_yt_xml
+from utils.reddit import reddit_make_post
 
 app = Flask(__name__)
 
@@ -12,12 +14,17 @@ def receive():
     request.get_data()
     data = request.data.decode("utf-8")
     if request.method == "POST":
-        title, link = parse_yt_xml(data)
-        print(title, link)
+        title, url = parse_yt_xml(data)
+        app.logger.info(f"found post: {title}, {url}, starting reddit subroutine")
+        reddit_make_post(subreddit=os.getenv("SUBREDDIT"), title=title, url=url, app=app)
         return "200"
     elif request.method == "GET":
         challenge = request.args["hub.challenge"]
         return challenge
 
 if __name__ == "__main__":
+    # if runnning locally, use the .env file
+    from dotenv import load_dotenv
+    load_dotenv()
     app.run()
+
