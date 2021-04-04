36a0a639458340857020529f342876d3a6e6f141
ecalzo <evancalz@me.com>
2021-04-04 14:09:16 -0400

    feat: retrieve channel_id from the xml

--- utils/xml_parser.py
+++ utils/xml_parser.py
@@ -5,12 +5,12 @@ def parse_yt_xml(xml_body: str) -> (str, str):
     entry = root.find("{http://www.w3.org/2005/Atom}entry")
     title = entry.find("{http://www.w3.org/2005/Atom}title") 
     link = entry.find("{http://www.w3.org/2005/Atom}link")
-
-    return title.text, link.attrib["href"]
+    channel_id = entry.find("{http://www.youtube.com/xml/schemas/2015}channelId")
+    return title.text, link.attrib["href"], channel_id.text
 
 
 if __name__ == "__main__":
     with open("test.xml", "r") as xml_file:
         text = xml_file.read()
-        root = ET.fromstring(text)
-        print(root)
+        title, link, channel_id = parse_yt_xml(text)
+        print(f"found title: {title}, link: {link}, channel_id: {channel_id}")
