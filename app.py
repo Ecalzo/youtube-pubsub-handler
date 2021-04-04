c5aef6446f691bc9b9541f2e1e09d9b172c96f1e
ecalzo <evancalz@me.com>
2021-04-04 14:19:14 -0400

    wip: psuedo code for implementing db lookup and handle multiple subreddits

--- app.py
+++ app.py
@@ -15,8 +15,11 @@ def receive():
     data = request.data.decode("utf-8")
     if request.method == "POST":
         title, url, channel_id = parse_yt_xml(data)
+        subreddits = query_for_sub_based_on_channel_id(channel_id)  # not implemented
         app.logger.info(f"found post: {title}, {url}, channel_id: {channel_id}. Starting reddit subroutine")
-        reddit_make_post(subreddit=os.getenv("SUBREDDIT"), title=title, url=url, app=app)
+        for subreddit in subreddits:
+            app.logger.info(f"making post to subreddit: {subreddit}")
+            reddit_make_post(subreddit=os.getenv("SUBREDDIT"), title=title, url=url, app=app)
         return "200"
     elif request.method == "GET":
         challenge = request.args["hub.challenge"]
