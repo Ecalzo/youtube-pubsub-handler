1461d5dcd454d28b9f00bc93560a9f5b4e16fa27
ecalzo <evancalz@me.com>
2021-04-15 20:14:02 -0400

    Changes to Data Models * use DateTime data type, plays nicely with python datetime * rename post.py to model.py * add function to create db

---
+++ models/models.py
@@ -0,0 +1,31 @@
+from app import db
+from datetime import datetime
+
+class Post(db.Model):
+    video_id = db.Column(db.String(), unique=True, nullable=False, primary_key=True)
+    channel_id = db.Column(db.String(), unique=False, nullable=False)
+    title = db.Column(db.String(), unique=True, nullable=False)
+    published = db.Column(db.DateTime(timezone=True))
+    updated = db.Column(db.DateTime(timezone=True))
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow())
+    updated_at = db.Column(db.DateTime(timezone=True))
+    
+    def __repr__(self):
+        return f"<Post {self.video_id}>" 
+
+
+class Lease(db.Model):
+    channel_id = db.Column(db.String(), unique=True, nullable=False primary_key=True)
+    lease_start_ts = db.Column(db.DateTime(timezone=True))
+    lease_expire_ts = db.Column(db.DateTime(timezone=True))
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow())
+    updated_at = db.Column(db.DateTime(timezone=True))
+    
+    def __repr__(self):
+        return f"<Lease {self.channel_id}"
+
+if __name__ == "__main__":
+    print("creating database")
+    db.create_all()
+    print("database created")
+
