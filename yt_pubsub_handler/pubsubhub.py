a572011b252c953312131f0d50e585d75556bb22
ecalzo <evancalz@me.com>
2021-04-18 16:42:25 -0400

    wip: blueprint implementation

--- yt_pubsub_handler/pubsubhub.py
+++ yt_pubsub_handler/pubsubhub.py
@@ -1,6 +1,8 @@
 from yt_pubsub_handler import db
 from flask import Blueprint, request, current_app
 from . import models
+from datetime import datetime, timedelta
+
 bp = Blueprint("pubsubhub", __name__, url_prefix="/pubsubhub")
 
 @bp.route("/hook", methods=("GET", "POST"))
@@ -8,13 +10,28 @@ def hook():
     request.get_data()
     data = request.data.decode("utf-8")
     if request.method == "GET":
+        args = request.args
         # get the data from the lease request
-        if models.Lease.query.filter(channel_id=request.args["channel_id"]
-        return "200"
+        channel_id = extract_channel_id(args["hub.topic"])
+        if models.Lease.query.filter_by(channel_id=channel_id):
+            # update the lease, or can we upsert???
+            # also handle unsubscribing here...
+            pass
+        else:  # this is a new lease
+            if args["hub.mode"] == "subscribe":
+                new_lease = models.Lease(
+                    channel_id=channel_id,
+                    lease_start_ts=datetime.utcnow(),
+                    lease_expire_ts=datetime.utcnow() + timedelta(seconds=int(args["hub.lease_seconds"]))
+                )
+                db.session.add(new_lease)
+                db.session.commit()
+        return args["hub.challenge"]
+
     if request.method == "POST":
-        error = None
-        if models.Post.query.filter_by(video_id=xml["video_id"]):
-           current_app.logger.info(f"video {xml.video_is} has already been posted")
+        xml = PSH_XML(data)
+        if models.Post.query.filter_by(video_id=xml.video_id):
+           current_app.logger.info(f"video {xml.video_id} has already been posted")
         else:
             current_app.logger.info(f"creating new post for video: {xml.title}, channel: {xml.channel_id}"
             new_post = models.Post(
@@ -29,7 +46,4 @@ def hook():
             # make post here
             return "200" 
 
-
-
-        
 
