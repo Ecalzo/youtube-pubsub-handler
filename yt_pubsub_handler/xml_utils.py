6845a239d2a87ce4e6d91bafeb24dd7d5d6e0fe1
ecalzo <evancalz@me.com>
2021-04-25 19:07:18 -0400

    fix: bugfixes for xml and date parsing

--- yt_pubsub_handler/xml_utils.py
+++ yt_pubsub_handler/xml_utils.py
@@ -9,13 +9,13 @@ class PSH_XML():
     def parse_yt_xml(self, xml_body: str): 
         root = ET.fromstring(xml_body)
         entry = root.find("{http://www.w3.org/2005/Atom}entry")
-        video_id = entry.find("{http://www.w3.org/2005/Atom}yt:videoId")
-        channel_id = entry.find("{http://www.w3.org/2005/Atom}yt:channelId")
-        title = entry.find("{http://www.w3.org/2005/Atom}title")
-        published = entry.find("{http://www.w3.org/2005/Atom}published")
-        updated = entry.find("{http://www.w3.org/2005/Atom}updated")
+        video_id = entry.find("{http://www.youtube.com/xml/schemas/2015}videoId").text
+        channel_id = entry.find("{http://www.youtube.com/xml/schemas/2015}channelId").text
+        title = entry.find("{http://www.w3.org/2005/Atom}title").text
+        published = entry.find("{http://www.w3.org/2005/Atom}published").text
+        updated = entry.find("{http://www.w3.org/2005/Atom}updated").text
         
-        published_dt, updated_dt = self._parse_xml_dates(published_dt, updated_dt)
+        published_dt, updated_dt = self._parse_xml_dates(published, updated)
         # accessors
         self.video_id = video_id
         self.channel_id = channel_id
@@ -24,14 +24,14 @@ class PSH_XML():
         self.updated = updated_dt
 
     def _parse_xml_dates(self, published_ts: str, updated_ts: str) -> (datetime.datetime, datetime.datetime):
-        pblsh_ts = datetime.datetime.strptime(published_ts, "%y-%m-%dt%h:%m:%s%z")
+        pblsh_ts = datetime.datetime.strptime(published_ts, "%Y-%m-%dT%H:%M:%S%z")
         updt = self._rm_seconds_from_ts(updated_ts)
-        updt_dt = datetime.datetime.strptime(updt, "%y-%m-%dt%h:%m:%s%z")
-        return pblsh_dt, updt_dt
+        updt_ts = datetime.datetime.strptime(updt, "%Y-%m-%dT%H:%M:%S%z")
+        return pblsh_ts, updt_ts
 
     def _rm_seconds_from_ts(self, ts: str) -> str:
         splt = ts.split(".")
         date_time = splt[0]
         tz_info = splt[-1].split("+")[-1]
-        return "+".join(date_time, tz_info)
+        return "+".join([date_time, tz_info])
 
