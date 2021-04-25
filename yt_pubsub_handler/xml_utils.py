17ee5e19d3f9e9d4af9ffbe3ad6858679f989a60
ecalzo <evancalz@me.com>
2021-04-25 12:21:01 -0400

    fix: make private funcs private

--- yt_pubsub_handler/xml_utils.py
+++ yt_pubsub_handler/xml_utils.py
@@ -15,7 +15,7 @@ class PSH_XML():
         published = entry.find("{http://www.w3.org/2005/Atom}published")
         updated = entry.find("{http://www.w3.org/2005/Atom}updated")
         
-        published_dt, updated_dt = parse_xml_dates(published_dt, updated_dt)
+        published_dt, updated_dt = self._parse_xml_dates(published_dt, updated_dt)
         # accessors
         self.video_id = video_id
         self.channel_id = channel_id
@@ -23,14 +23,15 @@ class PSH_XML():
         self.published = published_dt
         self.updated = updated_dt
 
-    def parse_xml_dates(published_ts: str, updated_ts: str):
+    def _parse_xml_dates(self, published_ts: str, updated_ts: str) -> (datetime.datetime, datetime.datetime):
         pblsh_ts = datetime.datetime.strptime(published_ts, "%y-%m-%dt%h:%m:%s%z")
         updt = self._rm_seconds_from_ts(updated_ts)
         updt_dt = datetime.datetime.strptime(updt, "%y-%m-%dt%h:%m:%s%z")
         return pblsh_dt, updt_dt
 
-    def _rm_seconds_from_ts(self, ts):
+    def _rm_seconds_from_ts(self, ts: str) -> str:
         splt = ts.split(".")
         date_time = splt[0]
         tz_info = splt[-1].split("+")[-1]
         return "+".join(date_time, tz_info)
+
