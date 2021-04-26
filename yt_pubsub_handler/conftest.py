d89ec6f2de476fbe70828daad71f51d8aa2d7f4b
ecalzo <evancalz@me.com>
2021-04-26 09:55:35 -0400

    tests: factory, subscriptions, and conftest config for pytest

---
+++ yt_pubsub_handler/conftest.py
@@ -0,0 +1,23 @@
+from yt_pubsub_handler import create_app
+from yt_pubsub_handler.db_utils import init_db_command
+from . import models
+import pytest
+
+@pytest.fixture
+def app():
+    app = create_app({
+        "TESTING": True
+    })
+
+    with app.app_context():
+        models.db.create_all()
+
+    yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
---
+++ yt_pubsub_handler/test_factory.py
@@ -0,0 +1,9 @@
+from yt_pubsub_handler import create_app
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({"TESTING": True}).testing
+
+def test_hello(client):
+    response = client.get("/hello")
+    assert response.data == b"Hello, World!"
