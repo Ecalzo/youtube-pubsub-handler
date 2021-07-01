4b7438fcbfef4b6f8109394fc8fdc837faab73bd
ecalzo <evancalz@me.com>
2021-07-01 08:09:38 -0400

    fix: allow renew lease func to work outside of the request context

--- yt_pubsub_handler/lease_utils.py
+++ yt_pubsub_handler/lease_utils.py
@@ -1,5 +1,6 @@
 import requests
-from flask import Flask, current_app, request
+import os
+from flask import Flask, current_app, request, has_request_context
 from datetime import datetime, timedelta
 from . import models
 
@@ -8,18 +9,19 @@ def renew_leases(app: Flask):
     # check for leases that expire in 24 hours
     # with yt_pubsub_handler.app_context():
     with app.app_context():
+        url_root = os.getenv("URL_ROOT")
         threshold = datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
         current_app.logger.info(f"querying for leases that expire before {threshold}")
         exp_leases = models.Lease.query.filter(models.Lease.lease_expire_ts < threshold)
         if exp_leases.first():
             for lease in exp_leases:
                 current_app.logger.info(f"renewing lease for https://youtube.com/channel/{lease.channel_id}")
-                request_new_lease(lease.channel_id)
+                request_new_lease(lease.channel_id, url_root=url_root)
         else:
             current_app.logger.info(f"no leases to renew, bye!")
 
 
-def request_new_lease(channel_id: str):
+def request_new_lease(channel_id: str, url_root: str = None):
     # helper function to renew a lease with pubsubhub
     headers = {
         "authority": "pubsubhubbub.appspot.com",
@@ -39,8 +41,15 @@ def request_new_lease(channel_id: str):
         "accept-language": "en-US,en;q=0.9",
     }
 
+    if url_root is not None:
+        root = url_root
+    elif has_request_context():
+        root = request.url_root
+    else:
+        raise Exception(f"No request context found, root provided: {url_root}")
+
     data = {
-        "hub.callback": f"{request.url_root}pubsubhub/hook",
+        "hub.callback": f"{root}pubsubhub/hook",
         "hub.topic": f"https://www.youtube.com/xml/feeds/videos.xml?channel_id={channel_id}",
         "hub.verify": "async",
         "hub.mode": "subscribe",
