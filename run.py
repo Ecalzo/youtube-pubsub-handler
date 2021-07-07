e85d175e8d8b3153de4eeda257131a101cd1d5ac
ecalzo <evancalz@me.com>
2021-07-07 08:09:01 -0400

    refactor: helper funcs for running migrations in an app context - for serverless deploy

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 from yt_pubsub_handler import create_app
 from yt_pubsub_handler.lease_utils import renew_leases
+from yt_pubsub_handler.db_utils import alembic_downgrade, alembic_migrate, alembic_upgrade
 
 app = create_app()
 
@@ -8,6 +9,18 @@ app = create_app()
 def run_renew_leases():
     url_root = os.getenv("URL_ROOT")
     renew_leases(app, url_root=url_root)
+
+
+def run_alembic_migrate():
+    alembic_migrate(app)
+
+
+def run_alembic_upgrade():
+    alembic_upgrade(app)
+
+
+def run_alembic_downgrade():
+    alembic_downgrade(app)
 
 
 if __name__ == "__main__":
